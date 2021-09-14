fa99725031827d9f4db41efd39ec4da94d380099
Perttu Ehn <perttu.ehn@yle.fi>
2021-09-14 15:17:26 +0300

    Init version of the action (severely incomplete)

---
+++ api.js
@@ -0,0 +1,319 @@
+import fetch from 'node-fetch';
+
+const apiBaseUrl = 'https://api.trello.com/1';
+const cache = {};
+const debug = process.env['TRELLO_ACTION_DEBUG'] || false;
+
+const boardId = () => {
+  if (!validateIdPattern(process.env['TRELLO_BOARD_ID'])) {
+    throw Error('Board ID is not valid.');
+  }
+  return process.env['TRELLO_BOARD_ID'];
+};
+
+/**
+ * Build API URI.
+ *
+ * @param {string} endpoint
+ * @returns string
+ */
+const buildApiUri = (endpoint) => `${apiBaseUrl}/${endpoint}`;
+
+/**
+ * Base headers for REST API  authentication et al.
+ *
+ * API access ie. apiKey and apiToken should be created in Trello as instructed on page
+ * https://developer.atlassian.com/cloud/trello/guides/rest-api/authorization/#authorizing-a-client
+ *
+ * @returns object
+ */
+const apiBaseHeaders = () => {
+  const apiKey = process.env['TRELLO_API_KEY'];
+  const apiToken = process.env['TRELLO_API_TOKEN'];
+  if (!apiKey || !apiToken) {
+    throw Error('Trello API key and/or token is missing.');
+  }
+
+  return {
+    Authorization: `OAuth oauth_consumer_key="${apiKey}", oauth_token="${apiToken}"`,
+    redirect: 'follow',
+    follow: 5,
+    Accept: 'application/json',
+    Method: 'GET',
+  };
+};
+
+/**
+ * Get Labels on a Board
+ *
+ * https://developer.atlassian.com/cloud/trello/rest/api-group-boards/#api-boards-id-labels-get
+ *
+ * @param {string} boardId
+ *
+ * @returns Object[]
+ */
+function getLabelsOfBoard() {
+  const endpoint = `boards/${boardId}/labels`;
+  const options = { ...apiBaseHeaders };
+  if (!cache.boardLabels[boardId]) {
+    cache.boardLabels[boardId] = new Promise(function (resolve, reject) {
+      if (debug) {
+        console.log(`getLabelsOfBoard calling ${buildApiUri(endpoint)} with`, options);
+      }
+      fetch(buildApiUri(endpoint), options)
+        .then((body) => {
+          if (debug) {
+            console.log(`getLabelsOfBoard got response:`, body.json());
+          }
+          cache.boardLabels[boardId] = resolve(body.json());
+          return cache.boardLabels[boardId];
+        })
+        .catch((error) => reject(error));
+    });
+  }
+  return cache.boardLabels[boardId];
+}
+
+/**
+ * Get the Members of a Board.
+ *
+ * https://developer.atlassian.com/cloud/trello/rest/api-group-boards/#api-boards-id-members-get
+ *
+ * @param {string} boardId
+ *
+ * @returns Object[]
+ */
+function getMembersOfBoard() {
+  const endpoint = `boards/${boardId}/members`;
+  const options = { ...apiBaseHeaders };
+  if (!cache.boardMembers[boardId]) {
+    cache.boardMembers[boardId] = new Promise(function (resolve, reject) {
+      if (debug) {
+        console.log(`getMembersOfBoard calling ${buildApiUri(endpoint)} with`, options);
+      }
+      fetch(buildApiUri(endpoint), options)
+        .then((body) => {
+          if (debug) {
+            console.log(`getMembersOfBoard got response:`, body.json());
+          }
+          cache.boardMembers[boardId] = resolve(body.json());
+          return cache.boardMembers[boardId];
+        })
+        .catch((error) => reject(error));
+    });
+  }
+  return cache.boardMembers[boardId];
+}
+
+/**
+ * Gets all (open) lists on Board.
+ *
+ * https://developer.atlassian.com/cloud/trello/guides/rest-api/nested-resources/#lists-nested-resource
+ *
+ * @param {string} boardId
+ *
+ * @returns Object[]
+ */
+function getListsOnBoard() {
+  // We are only interested in open lists.
+  const endpoint = `/object/${boardId}/lists??fields=all&filter==open`;
+  const options = { ...apiBaseHeaders };
+  if (!cache.boardLists[boardId]) {
+    cache.boardLists[boardId] = new Promise(function (resolve, reject) {
+      if (debug) {
+        console.log(`getListsOnBoard calling ${buildApiUri(endpoint)} with`, options);
+      }
+      fetch(buildApiUri(endpoint), options)
+        .then((body) => {
+          if (debug) {
+            console.log(`getListsOnBoard got response:`, body.json());
+          }
+          cache.boardLists[boardId] = resolve(body.json());
+          return cache.boardLists[boardId];
+        })
+        .catch((error) => reject(error));
+    });
+  }
+  return cache.boardLists[boardId];
+}
+
+/**
+ * Get Cards in a List.
+ *
+ * https://developer.atlassian.com/cloud/trello/rest/api-group-lists/#api-lists-id-board-get
+ *
+ * @param {*} listId
+ * @returns
+ */
+function getCardsOfList(listId) {
+  const endpoint = `lists/${listId}/cards`;
+  const options = { ...apiBaseHeaders };
+  if (!cache.listCards[listId]) {
+    cache.listCards[listId] = new Promise(function (resolve, reject) {
+      if (debug) {
+        console.log(`getListsOnBoard calling ${buildApiUri(endpoint)} with`, options);
+      }
+      fetch(buildApiUri(endpoint), options)
+        .then((body) => {
+          if (debug) {
+            console.log(`getListsOnBoard got response:`, body.json());
+          }
+          cache.listCards[listId] = resolve(body.json());
+          return cache.listCards[listId];
+        })
+        .catch((error) => reject(error));
+    });
+  }
+  return cache.listCards[listId];
+}
+/**
+ * Create a new Card
+ *
+ * https://developer.atlassian.com/cloud/trello/rest/api-group-cards/#api-cards-post
+
+ * @param {string} listId
+ * @param {*} params
+ * @returns Card
+ */
+function createCard(listId, params) {
+  const endpoint = `cards`;
+  const options = {
+    ...apiBaseHeaders,
+    method: 'POST',
+    url: buildApiUri(endpoint),
+    form: {
+      name: `[#${params.number}] ${params.title}`,
+      desc: params.description,
+      pos: 'bottom',
+      idList: listId,
+      urlSource: params.sourceUrl,
+      idMembers: params.memberIds,
+      idLabels: params.labelIds,
+    },
+    json: true,
+  };
+  return new Promise(function (resolve, reject) {
+    if (debug) {
+      console.log(`createCard calling: ${buildApiUri(endpoint)} with `, options);
+    }
+
+    fetch(buildApiUri(endpoint), options)
+      .then((body) => {
+        if (debug) {
+          console.log(`getListsOnBoard got response:`, body.json());
+        }
+        return resolve(body.json());
+      })
+      .catch((error) => reject(error));
+  });
+}
+/**
+ * Update the contents of a Card.
+ *
+ * https://developer.atlassian.com/cloud/trello/rest/api-group-cards/#api-cards-id-put
+ *
+ * @param {*} cardId
+ * @param {*} params
+ * @returns
+ */
+function updateCard(cardId, params) {
+  const endpoint = `cards/${cardId}`;
+  const options = {
+    ...apiBaseHeaders,
+    method: 'PUT',
+    form: {
+      idList: params.destinationListId,
+      idMembers: params.memberIds,
+    },
+  };
+  return new Promise(function (resolve, reject) {
+    if (debug) {
+      console.log(`updateCard calling: ${buildApiUri(endpoint)} with `, options);
+    }
+
+    fetch(buildApiUri(endpoint), options)
+      .then((body) => {
+        if (debug) {
+          console.log(`updateCard got response:`, body.json());
+        }
+        return resolve(body.json());
+      })
+      .catch((error) => reject(error));
+  });
+}
+
+/**
+ * Get Attachments on a Card
+ *
+ * https://developer.atlassian.com/cloud/trello/rest/api-group-cards/#api-cards-id-attachments-get
+ *
+ * @param {*} cardId
+ * @returns Attachment[]
+ */
+function getCardAttachments(cardId) {
+  const endpoint = `cards/${cardId}/attachments`;
+  const options = { ...apiBaseHeaders };
+
+  return new Promise(function (resolve, reject) {
+    if (debug) {
+      console.log(`getCardAttachments calling: ${buildApiUri(endpoint)} with `, options);
+    }
+
+    fetch(buildApiUri(endpoint), options)
+      .then((body) => {
+        if (debug) {
+          console.log(`getCardAttachments got response:`, body.json());
+        }
+        return resolve(body.json());
+      })
+      .catch((error) => reject(error));
+  });
+}
+
+/**
+ * Create Attachment On Card.
+ *
+ * https://developer.atlassian.com/cloud/trello/rest/api-group-cards/#api-cards-id-attachments-post
+ *
+ * @param {*} cardId
+ * @param {*} url
+ * @returns
+ */
+function addUrlSourceToCard(cardId, url) {
+  const endpoint = `cards/${cardId}/attachments`;
+  const options = {
+    ...apiBaseHeaders,
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+    },
+    form: {
+      url: url,
+    },
+  };
+  return new Promise(function (resolve, reject) {
+    if (debug) {
+      console.log(`addUrlSourceToCard calling: ${buildApiUri(endpoint)} with `, options);
+    }
+
+    fetch(buildApiUri(endpoint), options)
+      .then((body) => {
+        if (debug) {
+          console.log(`addUrlSourceToCard got response:`, body.json());
+        }
+        return resolve(body.json());
+      })
+      .catch((error) => reject(error));
+  });
+}
+
+export {
+  getLabelsOfBoard,
+  getMembersOfBoard,
+  getListsOnBoard,
+  getCardsOfList,
+  createCard,
+  updateCard,
+  getCardAttachments,
+  addUrlSourceToCard,
+};
---
+++ index.js
@@ -0,0 +1,187 @@
+import { setFailed, setOutput } from '@actions/core';
+import github from '@actions/github';
+import {
+  getLabelsOfBoard,
+  getMembersOfBoard,
+  getListsOnBoard,
+  getCardsOfList,
+  createCard,
+  updateCard,
+  getCardAttachments,
+  addUrlSourceToCard,
+} from './api';
+
+try {
+  const action = process.env['TRELLO_ACTION'];
+  if (!action) {
+    throw Error('Action is not set.');
+  }
+
+  switch (action) {
+    case 'issue_opened_create_card':
+      issueOpenedCreateCard();
+      break;
+    case 'pull_request_event_move_card':
+      pullRequestEventMoveCard();
+      break;
+
+    default:
+      throw Error('Action is not supported: ' + action);
+  }
+} catch (error) {
+  setFailed(error);
+}
+
+function issueOpenedCreateCard() {
+  try {
+    const listId = process.env['TRELLO_LIST_ID'];
+    validateListExistsOnBoard(listId);
+  } catch (error) {
+    setFailed(error);
+    return;
+  }
+
+  const issue = github.context.payload.issue;
+  const issueNumber = issue.number;
+  const issueTitle = issue.title;
+  const issueBody = issue.body;
+  const issueUrl = issue.html_url;
+  const issueAssigneeNicks = issue.assignees.map((assignee) => assignee.login);
+  const issueLabelNames = issue.labels.map((label) => label.name);
+
+  let trelloLabelIds = [];
+  let memberIds = [];
+
+  const labels = getLabelsOfBoard(boardId).then(function (response) {
+    const trelloLabels = response;
+    trelloLabels.filter((trelloLabel) => issueLabelNames.indexof(trelloLabel.name) !== -1);
+    trelloLabelIds.push(trelloLabels.map((label) => label.id));
+  });
+
+  const members = getMembersOfBoard(boardId).then(function (response) {
+    const members = response;
+    members.filter((member) => issueAssigneeNicks.indexof(member.username) !== -1);
+    memberIds.push(members.map((member) => member.id));
+  });
+
+  Promise.all([labels, members]).then(() => {
+    const cardParams = {
+      number: issueNumber,
+      title: issueTitle,
+      description: issueBody,
+      sourceUrl: issueUrl,
+      memberIds: memberIds.join(),
+      labelIds: trelloLabelIds.join(),
+    };
+
+    createCard(listId, cardParams).then((response) => {
+      if (debug)
+        console.log(
+          `createCard got response:`,
+          `Card created: [#${issueNumber}] ${issueTitle}`,
+          JSON.stringify(response, undefined, 2),
+        );
+    });
+  });
+}
+
+function pullRequestEventMoveCard() {
+  try {
+    const haystackList = process.env['TRELLO_SOURCE_LIST_ID'];
+    validateListExistsOnBoard(haystackList);
+
+    const targetList = process.env['TRELLO_TARGET_LIST_ID'];
+    validateListExistsOnBoard(targetList);
+
+    if (!haystackList || !targetList) {
+      throw Error("Trello's source and target list IDs must be present when moving card around.");
+    }
+  } catch (error) {
+    setFailed(error);
+    return;
+  }
+  const pullRequest = context.event.pull_request;
+
+  getMembersOfBoard(boardId)
+    .then(function (response) {
+      if (process.env['TRELLO_SYNC_BOARD_MEMBERS'] || false) {
+        const prReviewers = pullRequest.requested_reviewers.map((reviewer) => reviewer.login);
+        const members = response;
+        const additionalMemberIds = [];
+        prReviewers.forEach(function (reviewer) {
+          members.forEach(function (member) {
+            if (member.username == reviewer) {
+              additionalMemberIds[member.username] = member.id;
+            }
+          });
+        });
+        console.log('Additional members: ' + JSON.stringify(additionalMemberIds, undefined, 2));
+      }
+    })
+    .then(() => {
+      getCardsOfList(haystackList).then(function (response) {
+        const cards = response;
+        const prIssuesReferenced = pullRequest.body.match(/#[1-9][0-9]*/);
+        const prUrl = pullRequest.html_url;
+
+        let cardId;
+        let existingMemberIds = [];
+        cards.some(function (card) {
+          const haystack = `${card.name} ${card.desc}`;
+          const card_issue_numbers = haystack.match(/#[0-9][1-9]*/);
+          if (debug) {
+            console.log('card_issue_numbers', JSON.stringify(card_issue_numbers, undefined, 2));
+          }
+          // card_issue_numbers.forEach((element) => {});
+          // if (card_issue_number == prIssuesReferenced) {
+          //   cardId = card.id;
+          //   existingMemberIds = card.idMembers;
+          return false;
+          // }
+        });
+        const cardParams = {
+          destinationListId: targetList,
+          memberIds: existingMemberIds.concat(additionalMemberIds).join(),
+        };
+
+        if (cardId) {
+          updateCard(cardId, cardParams).then(function (response) {
+            getCardAttachments(cardId).then((response) => {
+              console.log('getCardAttachments response: ', JSON.stringify(response, undefined, 2));
+            });
+            addUrlSourceToCard(cardId, prUrl);
+          });
+        } else {
+          setFailed('Card not found.');
+        }
+      });
+    });
+}
+
+/**
+ * Validate Trello entity id.
+ *
+ * Trello ID's follow one pattern across all entities (cards, lists, boards).
+ *
+ * @param {string} id
+ *
+ * @returns boolean
+ */
+const validateIdPattern = (id) => id.match(/^[0-9a-fA-F]{24}$/);
+
+/**
+ * Validate Trello list exists on board.
+ *
+ * @param {string} listId
+ *
+ * @throws if lists is not on the board.
+ */
+const validateListExistsOnBoard = (listId) => {
+  if (!validateIdPattern(listId)) {
+    throw new Error('List id is not valid (pattern): ' + listId);
+  }
+  const lists = getListsOnBoard(boardId);
+  if (lists.indexOf(listId) === -1) {
+    throw new Error('List id is not on the board: ' + listId);
+  }
+};
