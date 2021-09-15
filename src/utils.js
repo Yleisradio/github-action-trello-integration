6237c8f992848bc4d2772333d10cdd99f226106b
Perttu Ehn <perttu.ehn@yle.fi>
2021-09-15 13:57:49 +0300

    Add more debugger to find process.env data

--- src/utils.js
+++ src/utils.js
@@ -29,13 +29,19 @@ const validateListExistsOnBoard = (listId) => {
 };
 
 const boardId = () => {
+  if (typeof process === 'undefined') {
+    return 'invalid 1';
+  }
+  if (typeof process.env === 'undefined') {
+    return 'invalid 2';
+  }
   if (typeof process.env.TRELLO_BOARD_ID === 'undefined') {
-    return 'invalid';
+    return 'invalid 3';
   }
   if (!validateIdPattern(process.env.TRELLO_BOARD_ID)) {
     throw Error('Board ID is not valid.');
   }
   return process.env.TRELLO_BOARD_ID;
 };
-
+console.debug(boardId, typeof boardId);
 export { validateIdPattern, validateListExistsOnBoard, boardId };
