053ad6552f5c58ce78fd1e920cb9a0b803999e91
Perttu Ehn <perttu.ehn@yle.fi>
2021-09-24 14:58:12 +0300

    Issue #1: Move octokit to global context, check it is set

--- src/api-github.ts
+++ src/api-github.ts
@@ -2,40 +2,42 @@ import * as github from '@actions/github';
 import { ghIssueCommentData } from './types';
 
 const debug: string | boolean = process.env.GITHUB_API_DEBUG || true;
+const githubToken: string | undefined = process.env.GITHUB_TOKEN;
+const octokit = githubToken && github.getOctokit(githubToken);
 
+/**
+ * Add comment to issue discussion (link to trello board)
+ */
 const addIssueComment = async ({
   comment,
   issueNumber,
   repoOwner,
   repoName,
 }: ghIssueCommentData): Promise<boolean> => {
-  const githubToken: string | undefined = process.env.GITHUB_TOKEN;
-  if (githubToken) {
-    const octokit = github.getOctokit(githubToken);
-
-    if (debug) {
-      console.debug('GH api / addIssueComment', {
-        issueNumber: issueNumber,
-        repoOwner: repoOwner,
-        repoName: repoName,
-      });
-    }
-    if (issueNumber && repoOwner && repoName) {
-      const commentData = {
-        body: comment,
-        issue_number: issueNumber,
-        owner: repoOwner,
-        repo: repoName,
-      };
-      const response = await octokit.rest.issues.createComment(commentData);
-      if (!response) {
-        console.error(`Octokit createComment() error with this issue. Data used:`, commentData);
-        return false;
-      }
-      return true;
-    }
+  if (!octokit) {
+    console.error('Octokit is not defined. Maybe GITHUB_TOKEN is not present or valid.');
+    return false;
   }
-  return false;
+  if (debug) {
+    console.debug('GH api / addIssueComment', {
+      issueNumber: issueNumber,
+      repoOwner: repoOwner,
+      repoName: repoName,
+    });
+  }
+
+  const commentData = {
+    body: comment,
+    issue_number: issueNumber,
+    owner: repoOwner,
+    repo: repoName,
+  };
+  const response = await octokit.rest.issues.createComment(commentData);
+  if (!response) {
+    console.error(`Octokit createComment() error with this issue. Data used:`, commentData);
+    return false;
+  }
+  return true;
 };
 
 export { addIssueComment };
