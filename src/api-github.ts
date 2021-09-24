df434ed11bd7aa830bde2f9bee4fc318aeb44e35
Perttu Ehn <perttu.ehn@yle.fi>
2021-09-24 14:30:25 +0300

    Issue #1: Update addIssueComment signature

--- src/api-github.ts
+++ src/api-github.ts
@@ -2,15 +2,17 @@ import * as github from '@actions/github';
 
 const debug: string | boolean = process.env.GITHUB_API_DEBUG || true;
 
-const addIssueComment = async (comment: string): Promise<boolean> => {
+const addIssueComment = async (
+  comment: string,
+  issueNumber: number | undefined,
+  repoOwner: string | undefined,
+  repoName: string | undefined,
+): Promise<boolean> => {
   const githubToken: string | undefined = process.env.GITHUB_TOKEN;
   if (githubToken) {
     const octokit = github.getOctokit(githubToken);
 
     const payload = github.context.payload;
-    const issueNumber: number | undefined = Number(payload.issue?.number) || undefined;
-    const repoOwner: string | undefined = payload.repository?.owner.name || undefined;
-    const repoName: string | undefined = payload.repository?.name;
     if (debug) {
       console.debug('GH api / addIssueComment', {
         issueNumber: issueNumber,
